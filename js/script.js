68b2979b929d8e59d67780e8a20b0502c7fed935
Prak-hyat <prakhyatbhattarai@gmail.com>
2026-04-12 20:06:50 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 function toggleMenu() {
-  const navList = document.querySelector("#main-nav ul");
-  navList.classList.toggle("show");
+  var navList = document.querySelector("#main-nav ul");
+  if (navList.style.display === "flex"){
+      navList.style.display = "none";
+  }else{
+  navList.style.display = "flex";
+  }
 }
-
 function toggleContent() {
-  const content = document.getElementById("extra-content");
+  var content = document.getElementById("extra-content");
+  var btn = event.target;
 
   if (content.style.display === "none" || content.style.display === "") {
     content.style.display = "block";
+    btn.textContent = "Show Less";
   } else {
     content.style.display = "none";
+    btn.textContent = "Show More Info";
   }
 }
